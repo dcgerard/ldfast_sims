de875b48be7a27ed9b1f4c52b2801e2e8b3a033f
David Gerard <gerard.1787@gmail.com>
2020-09-15 10:22:11 -0400

    added bw versions for all main figures

--- code/uit_largerr.R
+++ code/uit_largerr.R
@@ -78,3 +78,31 @@ ggsave(filename = "./output/uit/figs/mle_mom_nav.pdf",
        height = 5.5,
        width = 6,
        family = "Times")
+
+## black and white verstion
+mypoint <- function(data, mapping, ...) {
+  ggplot(data, mapping) +
+    geom_point() +
+    geom_abline(slope = 1, intercept = 0, lty = 2) +
+    xlim(0, 1) +
+    ylim(0, 1)
+}
+myhist <- function(data, mapping, ...) {
+  ggplot(data, mapping) +
+    geom_histogram(fill = "white", color = "black", bins = 20)
+}
+tibble(MLE = sub_cmle[upper.tri(sub_cmle)],
+       MoM = sub_cmom[upper.tri(sub_cmom)],
+       Naive = sub_cnav[upper.tri(sub_cnav)]) %>%
+  ggpairs(lower = list(continuous = mypoint),
+          diag = list(continuous = myhist),
+          upper = NULL) +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = "white")) ->
+  pl
+
+ggsave(filename = "./output/uit/figs/mle_mom_nav_bw.pdf",
+       plot = pl,
+       height = 5.5,
+       width = 6,
+       family = "Times")
